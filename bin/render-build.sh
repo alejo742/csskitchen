b83fc6ff8cb0f2d7680d8eed8e23a4a9767521e5
Alejandro Manrique <alejandromanriquenunez@gmail.com>
2024-12-14 01:00:30 -0500

    create: initial app setup for development deploy

---
+++ bin/render-build.sh
@@ -0,0 +1,8 @@
+set -o errexit
+
+bundle install
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+
+# because i have a free instance and can't migrate pre-start
+bundle exec rails db:migrate
