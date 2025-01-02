46c7e1cbd18638b5df4bc2e975dbc06c44b95b78
Alejandro Manrique <alejandromanriquenunez@gmail.com>
2025-01-01 19:31:30 -0500

    add: cook section dashboard responsiveness

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,12 @@ class ApplicationController < ActionController::Base
     end
 
     def current_user
-        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+        if session[:user_id]
+            unless @current_user
+                @current_user = User.find(session[:user_id])
+            end
+        end
+        return @current_user
     end
 
     def user_signed_in?
