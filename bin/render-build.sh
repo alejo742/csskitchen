f06b29e443653fcc0c5a66d631414dfc52e4050d
Alejandro Manrique <alejandromanriquenunez@gmail.com>
2025-01-04 19:08:59 -0500

    add: reset database on build for testing

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,4 @@ bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
 # because i have a free instance and can't migrate pre-start
-bundle exec rails db:migrate
+bundle exec rails db:reset
