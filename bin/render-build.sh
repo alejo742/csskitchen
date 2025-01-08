430a71025a33b81458aa5b579ec35c0906c8350b
Alejandro Manrique <alejandromanriquenunez@gmail.com>
2025-01-08 12:51:50 -0500

    fix: set database to migrate on deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,5 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-
-# because i have a free instance and can't migrate pre-start
-bundle exec rails db:drop
-bundle exec rails db:create
+bundle exec rails db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle exec rails db:migrate
