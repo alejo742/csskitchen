8b2b2f80825770f73aef70065acd4713392db0b9
Alejandro Manrique <alejandromanriquenunez@gmail.com>
2025-01-08 12:20:04 -0500

    fix: reset database on new commit push

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,6 @@ bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
 # because i have a free instance and can't migrate pre-start
-bundle exec rails db:reset
+bundle exec rails db:drop
+bundle exec rails db:create
+bundle exec rails db:migrate
