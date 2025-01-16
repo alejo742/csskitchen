6018db585ce4e6a4a141918caaf30036ea39ee5d
Alejandro Manrique <alejandromanriquenunez@gmail.com>
2025-01-15 20:39:40 -0500

    add: right panel and basic responsiveness for challenge view

--- app/javascript/controllers/challengeview_controller.js
+++ app/javascript/controllers/challengeview_controller.js
@@ -3,7 +3,6 @@ import * as monaco from 'monaco-editor';
 
 export default class extends Controller {
   connect() {
-    // toggling between tabs
     const orderTabs = document.querySelectorAll('.order-tab');
     orderTabs.forEach(tab => {
 		tab.addEventListener('click', () => {
@@ -12,7 +11,10 @@ export default class extends Controller {
 			toggleOrders(tab.innerHTML.toLowerCase());
 		});
     });
-    // make the toggles work
+    /*
+	* This code is used to toggle between the tabs in the order
+	* @param {string} status - The status of the order (urgent, completed)
+	*/
     const orders = document.querySelectorAll('.order');
     const toggleOrders = (status) => {
 		orders.forEach(order => {
@@ -39,27 +41,28 @@ export default class extends Controller {
 		},
 		fontSize: 14,
 		lineNumbersMinChars: 3,
+
+		acceptSuggestionOnEnter: 'on',
+		acceptSuggestionOnCommitCharacter: true,
+		wordBasedSuggestions: true,
+		quickSuggestions: true,
   	}
 
-	const htmlEditorElement = document.querySelector('.challenge-editor.html');
-	if (htmlEditorElement) {
-	  monaco.editor.create(htmlEditorElement, {
+	let htmlEditor = monaco.editor.create(document.querySelector('.challenge-editor.html'), {
 		...generalEditorSettings,
 		value: '<!-- Type your code here -->',
 		language: 'html',
-	  });
-	}
+	});
 
-	const cssEditorElement = document.querySelector('.challenge-editor.css');
-	if (cssEditorElement) {
-	  monaco.editor.create(cssEditorElement, {
+	let cssEditor = monaco.editor.create(document.querySelector('.challenge-editor.css'), {
 		...generalEditorSettings,
 		value: '/* Type your code here */',
 		language: 'css',
-	  });
-	}
+	});
 
-	// toggling between editors
+	/*
+	* This code is used to toggle between the tabs in the editor
+	*/
 	const editorTabs = document.querySelectorAll('.editor-tab');
 	editorTabs.forEach(tab => {
 		tab.addEventListener('click', () => {
@@ -67,6 +70,8 @@ export default class extends Controller {
 			editorTabs.forEach(tab => tab.classList.remove('active'));
 			tab.classList.add('active');
 
+			const htmlEditorElement = document.querySelector('.challenge-editor.html');
+			const cssEditorElement = document.querySelector('.challenge-editor.css');
 			// update editor language
 			if(tab.innerHTML.toLowerCase() === 'html') {
 				htmlEditorElement.classList.add('active');
@@ -77,6 +82,40 @@ export default class extends Controller {
 				cssEditorElement.classList.add('active');
 			}
 		});
+	});
+
+	/*
+	 * This code is used to update the food plate with the code from the editor
+	 */
+	function updateFoodPlate() {
+		const foodPlate = document.querySelector('.food-plate');
+        // create shadow root if it doesn't exist
+        if (!foodPlate.shadowRoot) {
+            foodPlate.attachShadow({ mode: 'open' });
+        }
+        const shadowRoot = foodPlate.shadowRoot;
+        const htmlContent = htmlEditor.getValue();
+        const cssContent = `<style>${cssEditor.getValue()}</style>`;
+        shadowRoot.innerHTML = cssContent + htmlContent;
+	}
+	// add event listeners to the editors to detect changes
+	htmlEditor.onDidChangeModelContent(updateFoodPlate);
+	cssEditor.onDidChangeModelContent(updateFoodPlate);
+
+	// initial update
+	updateFoodPlate();
+
+	// setup read-only editor for colors:
+	const colorEditor = monaco.editor.create(document.querySelector('.challenge-color-editor'), {
+		...generalEditorSettings,
+		value: '/* Brown */ \n rgba(128, 64, 48) \n /* Green */ \n rgba(0, 128, 0) \n /* Red */ \n rgba(255, 0, 0)',
+		language: 'css',
+		lineNumbers: "off",
+		glyphMargin: false,
+		folding: false,
+		readOnly: true,
+		lineDecorationsWidth: 0,
+		lineNumbersMinChars: 0,
 	});
   }
 }
