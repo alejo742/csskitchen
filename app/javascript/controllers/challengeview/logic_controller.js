9d5d2404112ec3f9d0f383c834fb0eddee1b7172
Alejandro Manrique <alejandromanriquenunez@gmail.com>
2025-01-22 22:08:15 -0500

    fix: erase relative import from controller

--- app/javascript/controllers/challengeview/logic_controller.js
+++ app/javascript/controllers/challengeview/logic_controller.js
@@ -1,9 +1,33 @@
 import { Controller } from "@hotwired/stimulus";
 import * as monaco from 'monaco-editor';
-import { showModal, hideModal } from './main_controller.js';
+
 
 export default class extends Controller {
     connect() {
+        /*
+        * This code is used to show a dynamic modal
+        * @param {string} title - The title of the modal
+        * @param {function} callback - The function to be attached to the positive button
+        */
+        function showModal(title, callback) {
+            // cleanup previous listeners
+            const positiveButton = document.querySelector('.popup-button.positive');
+            positiveButton.removeEventListener('click', callback);
+            const overlay = document.querySelector('.challenge-overlay');
+            const modalTitle = document.querySelector('.challenge-popup h2');
+            modalTitle.innerHTML = title;
+            overlay.classList.add('visible');
+
+            // add new listener
+            positiveButton.addEventListener('click', callback);
+        }
+        /*
+        * This code is used to hide the modal
+        */
+        function hideModal() {
+            const overlay = document.querySelector('.challenge-overlay');
+            overlay.classList.remove('visible');
+        }
 
         /*
         * This code is used to setup the editors for the challenge
@@ -267,5 +291,20 @@ export default class extends Controller {
                 }
             });
         });
+
+        /*
+        * This code is used to finish the challenge
+        */
+        const endButton = document.querySelector('.challenge-end-button');
+        endButton.addEventListener('click', () => {
+            showModal('Are you sure you want to end the challenge?', () => {
+                // make request to /dashboard route
+                window.location.href = '/dashboard';
+            });
+        });
+
+        // hide modal when pressing the negative button
+        const negativeButton = document.querySelector('.popup-button.negative');
+        negativeButton.addEventListener('click', hideModal);
     }
 }
